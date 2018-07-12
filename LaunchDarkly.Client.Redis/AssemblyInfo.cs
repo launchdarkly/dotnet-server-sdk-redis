32cb4f59baa577784c8281addc8f84218ab70340
Eli Bishop <eli@launchdarkly.com>
2018-07-12 15:37:49 -0700

    make our assembly strong-named

--- LaunchDarkly.Client.Redis/AssemblyInfo.cs
+++ LaunchDarkly.Client.Redis/AssemblyInfo.cs
@@ -2,12 +2,9 @@
 using System.Runtime.CompilerServices;
 
 // Allow unit tests to see internal classes
-[assembly: InternalsVisibleTo("LaunchDarkly.Client.Redis.Tests")]
-
-// Allow mock/proxy objects in unit tests to access internal classes
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=" +
-"0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99" +
-"c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654" +
-"753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46" +
-"ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484c" +
-"f7045cc7")]
+[assembly: InternalsVisibleTo("LaunchDarkly.Client.Redis.Tests, PublicKey=" +
+"0024000004800000940000000602000000240000525341310004000001000100" +
+"d528bbf5167350cea22550f00ee326746d6a6f046e7454e467a1cff0b24e390e" +
+"cb9a0fd59998b85210f9eb38ae0c9ca9e243c2854c0b6c1b242462e97f0e24d9" +
+"3e25c2ad82b72fee75c727934c16457302e514d2f407d5a77752e90071c8138c" +
+"25bec1ca436b505f41581dde42bef9e870362b4feb55a21685b57d586618779a")]
