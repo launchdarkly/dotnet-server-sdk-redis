e91e7c622a4b2ccf0b188833f228f0669072fa49
Eli Bishop <eli@launchdarkly.com>
2020-12-29 22:39:01 -0800

    Squashed 'dotnet-server-sdk-shared-tests/' content from commit 47a17fd
    git-subtree-dir: dotnet-server-sdk-shared-tests
    git-subtree-split: 47a17fd722317efd3c428c0897ea44236497f749

---
+++ LaunchDarkly.ServerSdk.SharedTests.Tests/DataStore/PersistentDataStoreBaseTestsSyncTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using LaunchDarkly.Sdk.Server.Interfaces;
+using Xunit.Abstractions;
+
+namespace LaunchDarkly.Sdk.Server.SharedTests.DataStore
+{
+    // This runs PersistentDataStoreBaseTests against a mock store implementation that is known to
+    // behave as expected, to verify that the test suite logic has the correct expectations.
+
+    public class PersistentDataStoreBaseTestsSyncTest : PersistentDataStoreBaseTests<MockSyncStore>
+    {
+        private readonly MockDatabase _database = new MockDatabase();
+
+        protected override PersistentDataStoreTestConfig<MockSyncStore> Configuration =>
+            new PersistentDataStoreTestConfig<MockSyncStore>
+            {
+                StoreFactoryFunc = CreateStoreFactory,
+                ClearDataAction = ClearAllData,
+            };
+
+        public PersistentDataStoreBaseTestsSyncTest(ITestOutputHelper testOutput) : base(testOutput) { }
+
+        private IPersistentDataStoreFactory CreateStoreFactory(string prefix) =>
+            new MockSyncStoreFactory { Database = _database, Prefix = prefix };
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        private async Task ClearAllData(string prefix) =>
+            _database.Clear(prefix);
+#pragma warning restore CS1998
+
+        private class MockSyncStoreFactory : IPersistentDataStoreFactory
+        {
+            internal MockDatabase Database { get; set; }
+            internal string Prefix { get; set; }
+
+            public IPersistentDataStore CreatePersistentDataStore(LdClientContext context) =>
+                new MockSyncStore(Database, Prefix);
+        }
+    }
+}
