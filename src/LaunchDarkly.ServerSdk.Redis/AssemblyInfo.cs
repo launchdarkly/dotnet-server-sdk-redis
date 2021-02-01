12c9d9787ab78346f590b515b7b3c3eae2272dac
Eli Bishop <eli@launchdarkly.com>
2021-02-01 11:23:49 -0800

    fix InternalsVisibleTo directive

--- src/LaunchDarkly.ServerSdk.Redis/AssemblyInfo.cs
+++ src/LaunchDarkly.ServerSdk.Redis/AssemblyInfo.cs
@@ -1,5 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// Allow unit tests to see internal classes
+#if DEBUG
+// Allow unit tests to see internal classes (note, the test assembly is not signed;
+// tests must be run against the Debug configuration of this assembly)
 [assembly: InternalsVisibleTo("LaunchDarkly.ServerSdk.Redis.Tests")]
+#endif
