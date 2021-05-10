5ad6628e88d732b647f6c599e29ae8fd01d6b50e
Eli Bishop <eli@launchdarkly.com>
2021-05-10 10:21:33 -0700

    Squashed 'dotnet-server-sdk-shared-tests/' changes from 3eff003..9d138db
    9d138db use latest prerelease SDK
    7bace38 fix target framework
    82197f0 update dependencies
    478cad3 update to latest prerelease API
    7d9d771 Merge branch 'sdk5.x'
    32eb2bf update shared test code for SDK 6.0 API (#2)
    
    git-subtree-dir: dotnet-server-sdk-shared-tests
    git-subtree-split: 9d138dbbc1265e4954c4a2eade90eeaddf408454

---
+++ LaunchDarkly.ServerSdk.SharedTests.Tests/DataStore/PersistentDataStoreBaseTestsAsyncTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using LaunchDarkly.Sdk.Server.Interfaces;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LaunchDarkly.Sdk.Server.SharedTests.DataStore
+{
+    // This runs PersistentDataStoreBaseTests against a mock store implementation that is known to
+    // behave as expected, to verify that the test suite logic has the correct expectations.
+
+    [Collection("Sequential")] // don't want this and the other test class to run simultaneously
+    public class PersistentDataStoreBaseTestsAsyncTest : PersistentDataStoreBaseTests
+    {
+        protected override PersistentDataStoreTestConfig Configuration =>
+            new PersistentDataStoreTestConfig
+            {
+                StoreAsyncFactoryFunc = CreateStoreFactory,
+                ClearDataAction = ClearAllData,
+            };
+
+        public PersistentDataStoreBaseTestsAsyncTest(ITestOutputHelper testOutput) : base(testOutput) { }
+
+        private IPersistentDataStoreAsyncFactory CreateStoreFactory(string prefix) =>
+            new MockAsyncStoreFactory { Database = MockDatabase.Instance, Prefix = prefix };
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        private async Task ClearAllData(string prefix) =>
+            MockDatabase.Instance.Clear(prefix);
+#pragma warning restore CS1998
+
+        private class MockAsyncStoreFactory : IPersistentDataStoreAsyncFactory
+        {
+            internal MockDatabase Database { get; set; }
+            internal string Prefix { get; set; }
+
+            public IPersistentDataStoreAsync CreatePersistentDataStore(LdClientContext context) =>
+                new MockAsyncStore(Database, Prefix);
+        }
+    }
+}
---
+++ LaunchDarkly.ServerSdk.SharedTests/DataStore/PersistentDataStoreTestConfig.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using LaunchDarkly.Sdk.Server.Interfaces;
+
+namespace LaunchDarkly.Sdk.Server.SharedTests.DataStore
+{
+    /// <summary>
+    /// Configuration for <see cref="PersistentDataStoreBaseTests{StoreT}"/>.
+    /// </summary>
+    /// either <c>IPersistentDataStore</c> or <c>IPersistentDataStoreAsync</c></typeparam>
+    public sealed class PersistentDataStoreTestConfig
+    {
+        /// <summary>
+        /// Set this to a function that takes a prefix string and returns a configured factory for
+        /// your implementation of <c>IPersistentDataStore</c>. If you implemented
+        /// <c>IPersistentDataStoreAsync</c> instead, then use <c>StoreAsyncFactoryFunc</c>.
+        /// </summary>
+        /// <remarks>
+        /// If the prefix string is null or "", it should use the default prefix defined by the
+        /// data store implementation. The factory must include any necessary configuration that
+        /// may be appropriate for the test environment (for instance, pointing it to a database
+        /// instance that has been set up for the tests).
+        /// </remarks>
+        public Func<string, IPersistentDataStoreFactory> StoreFactoryFunc { get; set; }
+
+        /// <summary>
+        /// Set this to a function that takes a prefix string and returns a configured factory for
+        /// your implementation of <c>IPersistentDataStoreAsync</c>. If you implemented
+        /// <c>IPersistentDataStore</c> instead, then use <c>StoreFactoryFunc</c>.
+        /// </summary>
+        /// <remarks>
+        /// If the prefix string is null or "", it should use the default prefix defined by the
+        /// data store implementation. The factory must include any necessary configuration that
+        /// may be appropriate for the test environment (for instance, pointing it to a database
+        /// instance that has been set up for the tests).
+        /// </remarks>
+        public Func<string, IPersistentDataStoreAsyncFactory> StoreAsyncFactoryFunc { get; set; }
+        
+        /// <summary>
+        /// Set this to an asynchronous function that removes all data from the underlying
+        /// data store for the specified prefix string.
+        /// </summary>
+        public Func<string, Task> ClearDataAction { get; set; }
+
+        /// <summary>
+        /// Set this to enable tests of concurrent modification behavior, for store implementations
+        /// that support testing this; otherwise leave it null.
+        /// </summary>
+        /// <remarks>
+        /// The function should take two parameters: an instance of your store type (typed here as
+        /// <c>object</c> because the actual implementation type is unknown to the tests), and a hook
+        /// which is a synchronous <c>Action</c>. Your function should modify the store instance
+        /// so that it will call the hook synchronously during each <c>Upsert</c> operation --
+        /// after the old value has been read, but before the new one has been written (if those
+        /// operations are not done atomically).
+        /// </remarks>
+        public Action<object, Action> SetConcurrentModificationHookAction { get; set; }
+    }
+}
