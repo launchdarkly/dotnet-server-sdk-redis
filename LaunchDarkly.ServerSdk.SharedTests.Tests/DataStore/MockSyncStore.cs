1deb42c8a717274405cf1d5c504a399fa7a227f2
Eli Bishop <eli@launchdarkly.com>
2021-05-28 17:24:57 -0700

    Squashed 'dotnet-server-sdk-shared-tests/' changes from 3eff003..007bc55
    007bc55 fix target framework in CI
    db9b789 add standard tests for big segment stores
    23ee6a6 update dependency to match what the SDK is using
    9d138db use latest prerelease SDK
    7bace38 fix target framework
    82197f0 update dependencies
    478cad3 update to latest prerelease API
    7d9d771 Merge branch 'sdk5.x'
    32eb2bf update shared test code for SDK 6.0 API (#2)
    
    git-subtree-dir: dotnet-server-sdk-shared-tests
    git-subtree-split: 007bc55a0e2f90370261288d4ed53b643cd9bd27

---
+++ LaunchDarkly.ServerSdk.SharedTests.Tests/DataStore/MockSyncStore.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using LaunchDarkly.Sdk.Server.Interfaces;
+
+using static LaunchDarkly.Sdk.Server.Interfaces.DataStoreTypes;
+
+namespace LaunchDarkly.Sdk.Server.SharedTests.DataStore
+{
+    public class MockSyncStore : IPersistentDataStore
+    {
+        private readonly MockDatabase _db;
+        private readonly string _prefix;
+
+        public MockSyncStore(MockDatabase db, string prefix)
+        {
+            _db = db;
+            _prefix = prefix ?? "";
+        }
+
+        public void Dispose() { }
+
+        public SerializedItemDescriptor? Get(DataKind kind, string key) =>
+            _db.DataForPrefixAndKind(_prefix, kind).TryGetValue(key, out var ret) ?
+                ret : (SerializedItemDescriptor?)null;
+
+        public KeyedItems<SerializedItemDescriptor> GetAll(DataKind kind) =>
+            new KeyedItems<SerializedItemDescriptor>(_db.DataForPrefixAndKind(_prefix, kind));
+
+        public void Init(FullDataSet<SerializedItemDescriptor> allData)
+        {
+            _db.DataForPrefix(_prefix).Clear();
+            foreach (var coll in allData.Data)
+            {
+                _db.DataForPrefix(_prefix)[coll.Key] = new Dictionary<string, SerializedItemDescriptor>(coll.Value.Items);
+            }
+            _db.SetInited(_prefix);
+        }
+
+        public bool Initialized() => _db.Inited(_prefix);
+
+        public bool IsStoreAvailable() => true;
+
+        public bool Upsert(DataKind kind, string key, SerializedItemDescriptor item)
+        {
+            var dict = _db.DataForPrefixAndKind(_prefix, kind);
+            if (dict.TryGetValue(key, out var oldItem) && oldItem.Version >= item.Version)
+            {
+                return false;
+            }
+            dict[key] = item;
+            return true;
+        }
+    }
+}
